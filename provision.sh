c7c763f482071849dc3d19ae4c84cd6fa32001ee
shazid98 <shazid.tipu@gmail.com>
2022-06-20 16:13:13 +0100

    updated readme

--- provision.sh
+++ provision.sh
@@ -1,31 +1,89 @@
 #!/bin/bash
 
-# update
-sudo apt-get update -y
+#update & upgrade
+sudo apt update -y
 
-# upgrade
-sudo apt-get upgrade -y
+sudo apt upgrade -y
 
 # install nginx
-sudo apt-get install nginx -y
+sudo apt install nginx -y
 
-# start nginx
-sudo systemctl start nginx
 
 # enable nginx
 sudo systemctl enable nginx
 
-# download python stuff
-sudo apt-get install python-software-properties -y
 
-# download v6
+# start nginx
+sudo systemctl start nginx
+
+
+
+# install nodejs v6
 sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 
-# install node-js
-sudo apt-get install nodejs -y
+
+
+# install nodejs part 2
+sudo apt install nodejs -y
+sudo apt install python-software-properties -y
+
+
 
 # install pm2
 sudo npm install pm2 -g
+
+
+
+# copy default into sites-available and enabling nginx to work again with the new default file
+# sudo rm -rf etc/nginx/sites-available/default
+# sudo cp default /etc/nginx/sites-available/
+# sudo nginx -t
+sudo systemctl restart nginx
+sudo systemctl enable nginx
+
+
+
+# Adding env variable
+# sudo echo "export DB_HOST=mongodb://192.168.10.150:27017/posts" >> .bashrc
+# source .bashrc
+
+
+
+# cd into app and install and start app
+# cd app/app/
+# npm install
+# nohup node app.js > /dev/null 2>&1 &
+
+
+
+# #!/bin/bash
+
+# # update
+# sudo apt-get update -y
+
+# # upgrade
+# sudo apt-get upgrade -y
+
+# # install nginx
+# sudo apt-get install nginx -y
+
+# # start nginx
+# sudo systemctl start nginx
+
+# # enable nginx
+# sudo systemctl enable nginx
+
+# # download python stuff
+# sudo apt-get install python-software-properties -y
+
+# # download v6
+# sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
+# # install node-js
+# sudo apt-get install nodejs -y
+
+# # install pm2
+# sudo npm install pm2 -g
 
 # sudo echo "export DB_HOST=mongodb://192.168.10.150:27017/posts" >> ~/.bashrc
 
