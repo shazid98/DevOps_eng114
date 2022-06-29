c89ddcf5e2022c02f4aa13cf64ac000bb1feb1c6
shazid98 <shazid.tipu@gmail.com>
2022-06-29 18:03:13 +0100

    update

--- playbooks_provision.sh
+++ playbooks_provision.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-sudo mv DevOps_eng114/playbooks/nginx-playbook.yml /etc/ansible
-sudo mv DevOps_eng114/playbooks/reverse-proxy-playbook.yml /etc/ansible
-sudo mv DevOps_eng114/playbooks/node-playbook.yml /etc/ansible
-sudo mv DevOps_eng114/playbooks/npm-playbook.yml /etc/ansible
-sudo mv DevOps_eng114/playbooks/mongodb-playbook.yml /etc/ansible
-sudo mv DevOps_eng114/playbooks/copy-playbook.yml /etc/ansible
-sudo mv DevOps_eng114/playbooks/app-run-playbook.yml /etc/ansible
+# sudo mv DevOps_eng114/playbooks/nginx-playbook.yml /etc/ansible
+# sudo mv DevOps_eng114/playbooks/reverse-proxy-playbook.yml /etc/ansible
+# sudo mv DevOps_eng114/playbooks/node-playbook.yml /etc/ansible
+# sudo mv DevOps_eng114/playbooks/npm-playbook.yml /etc/ansible
+# sudo mv DevOps_eng114/playbooks/mongodb-playbook.yml /etc/ansible
+# sudo mv DevOps_eng114/playbooks/copy-playbook.yml /etc/ansible
+# sudo mv DevOps_eng114/playbooks/app-run-playbook.yml /etc/ansible
 
 cd /etc/ansible
-ansible-playbook nginx-playbook.yml
-ansible-playbook reverse-proxy-playbook.yml
-ansible-playbook node-playbook.yml
-ansible-playbook npm-playbook.yml
-ansible-playbook mongodb-playbook.yml
-ansible-playbook copy-playbook.yml
-ansible-playbook app-run-playbook.yml
+sudo ansible-playbook nginx-playbook.yml --ask-vault-pass
+sudo ansible-playbook reverse-proxy-playbook.yml --ask-vault-pass
+sudo ansible-playbook node-playbook.yml --ask-vault-pass
+sudo ansible-playbook npm-playbook.yml --ask-vault-pass
+sudo ansible-playbook copy-playbook.yml --ask-vault-pass
+sudo ansible-playbook mongodb-playbook.yml --ask-vault-pass
+sudo ansible-playbook app-run-playbook.yml --ask-vault-pass
+
