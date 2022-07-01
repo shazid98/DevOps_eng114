7edf8cc0b29acce9b2185a33e439d5a2799ffa12
shazid98 <shazid.tipu@gmail.com>
2022-07-01 15:12:27 +0100

    update

--- eng114_terraform/main.tf
+++ eng114_terraform/main.tf
@@ -26,7 +26,7 @@ resource "aws_internet_gateway" "eng114_shazid_ig" {
 
 resource "aws_subnet" "eng114_shazid_terraform_public_subnet" {
   vpc_id     = aws_vpc.eng114_shazid_terraform.id
-  cidr_block = "10.34.1.0/24"
+  cidr_block = "10.34.2.0/24"
   map_public_ip_on_launch = true
   availability_zone = "eu-west-1a"
 
@@ -37,8 +37,8 @@ resource "aws_subnet" "eng114_shazid_terraform_public_subnet" {
 
 resource "aws_subnet" "eng114_shazid_terraform_private_subnet" {
   vpc_id     = aws_vpc.eng114_shazid_terraform.id
-  cidr_block = "10.34.10.0/24"
-  map_public_ip_on_launch = true
+  cidr_block = "10.34.3.0/24"
+  map_public_ip_on_launch = false
   availability_zone = "eu-west-1a"
 
   tags = {
@@ -46,7 +46,7 @@ resource "aws_subnet" "eng114_shazid_terraform_private_subnet" {
   }
 }
 
-########################################### Route table (public)
+# Route table (public)
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.eng114_shazid_terraform.id
   tags = {
@@ -54,7 +54,7 @@ resource "aws_route_table" "public" {
   }
 }
 
-########################################### Route from (public)
+# Route from (public)
 resource "aws_route" "public_internet_gateway" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
@@ -62,16 +62,16 @@ resource "aws_route" "public_internet_gateway" {
 }
 
 
-################################################ Subnet assosiations (public)
+# Subnet associations (public)
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.eng114_shazid_terraform_public_subnet.id
   route_table_id = aws_route_table.public.id
 }
 
 # Security Groups
-resource "aws_security_group" "security_ports" {
-  name = "ports"
-  description = "Allow port 80"
+resource "aws_security_group" "app_security_ports" {
+  name = "app_ports"
+  description = "Allow ports"
   vpc_id = aws_vpc.eng114_shazid_terraform.id
 
   ingress {
@@ -87,6 +87,13 @@ resource "aws_security_group" "security_ports" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 3000
+    protocol  = "tcp"
+    to_port   = 3000
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     protocol  = "-1"
@@ -95,24 +102,106 @@ resource "aws_security_group" "security_ports" {
   }
 
   tags = {
-    Name = "ports"
+    Name = "app_ports"
+  }
+}
+
+resource "aws_security_group" "db_security_ports" {
+  name = "db_ports"
+  description = "Allow SSH"
+  vpc_id = aws_vpc.eng114_shazid_terraform.id
+
+  ingress {
+    from_port = 22
+    protocol  = "tcp"
+    to_port   = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "db_ports"
+  }
+}
+
+resource "aws_security_group" "controller_security_ports" {
+  name = "controller_ports"
+  description = "Allow port 80"
+  vpc_id = aws_vpc.eng114_shazid_terraform.id
+
+  ingress {
+    from_port = 22
+    protocol  = "tcp"
+    to_port   = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "controller_ports"
   }
 }
 
 
-resource "aws_instance" "app_instance"{
+
+resource "aws_instance" "controller_instance"{
 # choose your ami and instance type
-    ami = "ami-0b47105e3d7fc023e"
+    ami = "${var.node_controller_id}"
     instance_type = "t2.micro"
+    key_name = "${var.aws_key_name}"
     subnet_id = "${aws_subnet.eng114_shazid_terraform_public_subnet.id}"
-    vpc_security_group_ids = [aws_security_group.security_ports.id]
+    vpc_security_group_ids = [aws_security_group.controller_security_ports.id]
 
 # enable a public ip
     associate_public_ip_address = true
 
 # name the instance
     tags = {
-        Name = "eng114_shazid_terraform"
+        Name = "eng114_shazid_terraform_controller"
     }
 }
 
+resource "aws_instance" "db_instance"{
+# choose your ami and instance type
+    ami = "${var.node_db_id}"
+    instance_type = "t2.micro"
+    key_name = "${var.aws_key_name}"
+    subnet_id = "${aws_subnet.eng114_shazid_terraform_private_subnet.id}"
+    vpc_security_group_ids = [aws_security_group.db_security_ports.id]
+
+# enable a public ip
+    associate_public_ip_address = false
+
+# name the instance
+    tags = {
+        Name = "eng114_shazid_terraform_db"
+    }
+}
+resource "aws_instance" "app_instance"{
+# choose your ami and instance type
+    ami = "${var.node_app_id}"
+    instance_type = "t2.micro"
+    key_name = "${var.aws_key_name}"
+    subnet_id = "${aws_subnet.eng114_shazid_terraform_public_subnet.id}"
+    vpc_security_group_ids = [aws_security_group.app_security_ports.id]
+
+# enable a public ip
+    associate_public_ip_address = true
+
+# name the instance
+    tags = {
+        Name = "eng114_shazid_terraform_app"
+    }
+}
---
+++ eng114_terraform/variable.tf
@@ -0,0 +1,20 @@
+variable "node_app_id"{
+    default = "ami-05490ce6657881403"
+}
+
+variable "node_db_id"{
+    default = "ami-07db711042ce9bf55"
+}
+
+variable "node_controller_id"{
+    default = "ami-0e8df5fc47976d293"
+}
+
+variable "aws_key_name"{
+    default = "eng114_group"
+}
+
+variable "aws_key_path"{
+    default = "~/.ssh/eng114_group.pem"
+}
+
