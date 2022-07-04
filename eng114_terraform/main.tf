575c7320cbd6f5f2fb758ed233f0adc70acabcdf
shazid98 <shazid.tipu@gmail.com>
2022-07-04 12:25:54 +0100

    update

--- eng114_terraform/main.tf
+++ eng114_terraform/main.tf
@@ -161,71 +161,57 @@ resource "aws_security_group" "controller_security_ports" {
   }
 }
 
-# Creating load balancer for the app
-resource "aws_elb" "app_lb" {
-  name = "eng114-shazid-lb"
-  security_groups = ["${aws_security_group.app_security_ports.id}"]
-  subnets = ["${aws_subnet.eng114_shazid_terraform_public_subnet.id}"]
-  cross_zone_load_balancing   = true
-  
-  health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
-    interval = 30
-    target = "HTTP:80/"
-  }
-  
-  listener {
-    lb_port = 80
-    lb_protocol = "http"
-    instance_port = "80"
-    instance_protocol = "http"
-   }
-  }
+# Controller instance
+resource "aws_instance" "controller_instance"{
+# choose your ami and instance type
+    ami = "${var.node_controller_id}"
+    instance_type = "t2.micro"
+    key_name = "${var.aws_key_name}"
+    subnet_id = "${aws_subnet.eng114_shazid_terraform_public_subnet.id}"
+    vpc_security_group_ids = [aws_security_group.controller_security_ports.id]
 
+# enable a public ip
+    associate_public_ip_address = true
 
-# Creating launch template
-  resource "aws_launch_configuration" "app" {
-  name_prefix = "eng114-shazid-terraform-asg-app-"
-  image_id = "${var.node_app_id}" 
-  instance_type = "t2.micro"
-  key_name = "${var.aws_key_name}"
-  security_groups = [ "${aws_security_group.app_security_ports.id}" ]
-  associate_public_ip_address = true
-  
-  lifecycle {
-    create_before_destroy = true
-  }
+# name the instance
+    tags = {
+        Name = "eng114_shazid_terraform_controller"
+    }
 }
 
+# DB instance
+resource "aws_instance" "db_instance"{
+# choose your ami and instance type
+    ami = "${var.node_db_id}"
+    instance_type = "t2.micro"
+    key_name = "${var.aws_key_name}"
+    subnet_id = "${aws_subnet.eng114_shazid_terraform_private_subnet.id}"
+    vpc_security_group_ids = [aws_security_group.db_security_ports.id]
 
-# Creating an autoscaling group
-resource "aws_autoscaling_group" "app" {
-  name = "eng114-shazid-terraform-asg-app"
-  min_size             = 2
-  desired_capacity     = 2
-  max_size             = 3
-  
+# enable a public ip
+    associate_public_ip_address = false
 
-  load_balancers = ["${aws_elb.app_lb.id}"]
-  launch_configuration = "${aws_launch_configuration.app.name}"
-  enabled_metrics = [
-    "GroupMinSize",
-    "GroupMaxSize",
-    "GroupDesiredCapacity",
-    "GroupInServiceInstances",
-    "GroupTotalInstances"
-  ]
-  metrics_granularity = "1Minute"
-  vpc_zone_identifier  = ["${aws_subnet.eng114_shazid_terraform_public_subnet.id}"]
-  lifecycle {
-    create_before_destroy = true
-  }
-  tag {
-    key                 = "Name"
-    value               = "eng114-shazid-terraform-app"
-    propagate_at_launch = true
-  }
-  
-  }
+# name the instance
+    tags = {
+        Name = "eng114_shazid_terraform_db"
+    }
+}
+
+#App Instance
+resource "aws_instance" "app_instance"{
+
+    ami = "${var.node_app_id}"
+    instance_type = "t2.micro"
+    key_name = "${var.aws_key_name}"
+    subnet_id = "${aws_subnet.eng114_shazid_terraform_public_subnet.id}"
+    vpc_security_group_ids = [aws_security_group.app_security_ports.id]
+
+    # enable a public ip
+    associate_public_ip_address = true
+
+    # name the instance
+    tags = {
+        Name = "eng114_shazid_terraform_app"
+    }
+}
+
